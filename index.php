1b466bc54b0bae8708da6d3a8e143e9de99b2ede
Max Brants <max.brants@gmail.com>
2020-02-29 01:49:44 +0100

    test

--- index.php
+++ index.php
@@ -10,9 +10,7 @@ include 'includes/header.php';
 ?>
 <link rel="stylesheet" href="css\index.css">
 <div id="informatie">
-        <h1>dit is een test of de opacity goed werkt</h1>
-        <h2>dit is een test of de opacity goed werkt</h2>
-        <p>dit is een test of de opacity goed werkt</p>
+
 </div>
 <?php
 include 'includes/footer.php';
