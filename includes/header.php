7beccd73c8e002b332f0defc02c3ffb90c3abd4c
Max Brants <max.brants@gmail.com>
2020-02-29 01:31:16 +0100

    added form

--- includes/header.php
+++ includes/header.php
@@ -13,30 +13,30 @@
     <meta name="viewport" content="width=device-width initial-scale=1,0" />
     <title>CV Ketel Udenhout</title>
 </head>
+<link rel="stylesheet" href="css/grt-responsive-menu.css">
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT"
+        crossorigin="anonymous">
+</script>
+<script src="js/grt-responsive-menu.js"></script>
 <body background="img/headerImg.jpg">
-<div id="header" >
-    <div id="logo">
-        <a href="index.php"> <img alt="logo" src="img/logo CV Ketel.png" /> </a>
+<header>
+    <div class="menu-container">
+        <div class="grt-menu-row">
+            <div class="grt-menu-logo">
+                <a href="index"  class="grt-logo"><img src="img/logo CV Ketel.png"></a>
+            </div>
+            <div class="grt-menu-right">
+                <nav>
+                    <button class="grt-mobile-button"><span class="line1"></span><span class="line2"></span><span class="line3"></span></button>
+                    <ul class="grt-menu">
+                        <li><a href="index">Home</a></li>
+                        <li><a href="inschrijven">inschrijven</a></li>
+                        <li><a href="tijdlijn">tijdlijn</a></li>
+                        <li><a href="sponsors">sponsors</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
     </div>
-    <ul id="name">
-        <li><a href="">CV Ketel Udenhout</a></li>
-    </ul>
-    <ul id="navbar">
-        <li><a href="index.php">Home</a> <ul>
-            </ul> </li>
-        <li><a href="social media.php">Social media</a> <ul>
-                <li> <a href="https://www.facebook.com/cvketeludenhout/">Facebook</a></li>
-                <li> <a href="https://www.instagram.com/cv_ketel_udenhout/">Instagram</a></li>
-                <li> <a href="snapchat.php">Snapchat</a></li>
-            </ul> </li>
-        <li><a href="tickets.php">kaarten kopen</a> <ul>
-            </ul> </li>
-        <li><a href="sponsors.php">sponsors</a> <ul>
-            </ul> </li>
-        <li><a href="tijdlijn.php">Tijdlijn</a> <ul>
-                <li> <a href="carnaval2017.php">2017</a> </li>
-                <li> <a href="carnaval2018.php">2018</a> </li>
-                <li> <a href="carnaval2019.php">2019</a> </li>
-            </ul> </li>
-    </ul>
-</div>
+</header>
